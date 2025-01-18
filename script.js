eda8430c88c3c252a26dd5a558d5231333675dc7
Your Name <you@example.com>
2025-01-18 11:29:44 +0100

    Popis změn

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const button = document.getElementById('generateButton');
 const numberDisplay = document.getElementById('number');
 
 button.addEventListener('click', () => {
+  generateRandomNumber();
+});
+
+function generateRandomNumber() {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   numberDisplay.textContent = `Generated number: ${randomNumber}`;
-});
+}
