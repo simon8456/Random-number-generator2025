22f460b55451e61399833b39bbc21120709644ea
Your Name <you@example.com>
2025-01-18 12:46:04 +0100

    Popis změn

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
